dc189abe90d30d4bcccd41de035681eb99587f87
setyohandoko <setyohandoko@client-Setyo-Handoko.local>
2023-12-13 00:58:13 +0700

    New Version V.2

--- checkout.js
+++ checkout.js
@@ -2,6 +2,53 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
 // Get specific parameter values by key
-const orderId = urlParams.get('orderId'); // Retrieves 'value1'
+const promoCode = urlParams.get('promoCode'); // Retrieves 'promoCode'
 const orderIdDom = document.querySelector('#order-id');
-orderIdDom.innerText = orderId;
+
+const handlerSubmit = (e) => {
+  window.dataLayer = window.dataLayer || [];
+  const orderId = generateRandomString(5);
+  const payload = {
+    orderId,
+    orderDiscount: generateRandomDecimal(0, 10),
+    promoCode,
+    items: [
+      {
+        category: 'HEADSET',
+        sku: generateRandomString(13),
+        quantity: 5,
+        subtotal: generateRandomDecimal(0, 10),
+      },
+      {
+        category: 'HEADSET',
+        sku: generateRandomString(13),
+        quantity: 10,
+        subtotal: generateRandomDecimal(0, 10),
+      },
+    ],
+  };
+
+  console.log('payload');
+  console.log(payload);
+  dataLayer.push(payload);
+  orderIdDom.innerText = orderId;
+};
+
+document.addEventListener('DOMContentLoaded', handlerSubmit);
+
+function generateRandomString(length) {
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+function generateRandomDecimal(min, max) {
+  return (Math.random() * (max - min) + min).toFixed(2);
+}
